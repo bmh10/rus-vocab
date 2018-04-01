72c692189b91f27b6e5d4c06c7b3427358b5e3ac
bmh10 <bmhomer13@gmail.com>
2018-04-01 09:40:40 +0100

    Track correct vs incorrect

--- Main.hs
+++ Main.hs
@@ -6,22 +6,30 @@ import Data.Fixed
 import Data.List
 import Data.Maybe
 
-test [] = do
-  putStrLn "Lesson completed"
+data Meta = Meta { correct :: Int, total :: Int } deriving (Eq, Show)
 
-test (v:vs) = do
+updateMeta meta isCorrect =
+  if isCorrect then meta { correct = (correct meta) + 1} else meta
+
+test [] meta = do
+  putStrLn $ "Lesson completed, you scored " ++ show (correct meta) ++ "/" ++ show ()
+
+test (v:vs) meta = do
   putStrLn $ "What is the English for " ++ rus ++ "?"
   ans <- getLine
-  if ans == eng 
+  let isCorrect = ans == eng
+  if isCorrect
     then putStrLn "Correct"
     else putStrLn $ "Incorrect, the correct answer was '" ++ eng ++ "'"
 
-  test vs
+  test vs (updateMeta meta isCorrect)
   where
     ws  = words v
     rus = ws !! 0
     eng = ws !! 1 
 
 main = do
-  vocab <- readFile "vocab/top100.txt"
-  test $ lines vocab 
+  fileContents <- readFile "vocab/top100.txt"
+  let vocab = lines fileContents
+  let meta = Meta { correct = 0, total = length vocab }
+  test vocab meta
