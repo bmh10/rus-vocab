4a742f219cfd7373b46d3af598fe8fea04e95b43
bmh10 <bmhomer13@gmail.com>
2018-04-01 09:15:17 +0100

    Basic vocab test

--- Main.hs
+++ Main.hs
@@ -6,6 +6,22 @@ import Data.Fixed
 import Data.List
 import Data.Maybe
 
+test [] = do
+  putStrLn "Lesson completed"
+
+test (v:vs) = do
+  putStrLn $ "What is the English for " ++ rus ++ "?"
+  ans <- getLine
+  if ans == eng 
+    then putStrLn "Correct"
+    else putStrLn $ "Incorrect, the correct answer was '" ++ eng ++ "'"
+
+  test vs
+  where
+    ws  = words v
+    rus = ws !! 0
+    eng = ws !! 1 
+
 main = do
   vocab <- readFile "vocab/top100.txt"
-  putStrLn vocab
+  test $ lines vocab 
