8b6997d66d95f6e1bea43df1e7dcf68f4d48e90f
bmh10 <bmhomer13@gmail.com>
2018-04-02 09:47:04 +0100

    Extract top 100 russian words

--- vocab/extract.sh
+++ vocab/extract.sh
@@ -2,11 +2,13 @@
 
 for i in {1..5}
 do
-  wget -O orig$i https://www.russianpod101.com/russian-word-lists/?page=$i
+  wget -q -O orig$i https://www.russianpod101.com/russian-word-lists/?page=$i
 
   cat orig$i | grep js-wlv-english | cut -d'>' -f2 | cut -d'<' -f1 > eng$i
   cat orig$i | grep "js-wlv-word\" lang=\"ru\"" | cut -d'>' -f2 | cut -d'<' -f1 > ru$i
 
-  paste -d" " ru$i eng$i > final$i
+  paste -d" " ru$i eng$i >> top100.txt
   rm -rf orig$i eng$i ru$i
+  echo -n .
 done
+
